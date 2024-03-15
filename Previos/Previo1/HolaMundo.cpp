5e9d0717a83515249169a589aca42b4a8f8c6970
Jorge Meneses <menesesjorge2003@gmail.com>
2024-03-14 22:58:14 -0600

    Previo 1: Dos archivos basicos que imprimen texto

---
+++ Previos/Previo1/HolaMundo.cpp
@@ -0,0 +1,8 @@
+// Programa mas basico posible de C++
+#include <iostream> // Importar libreria basica de input - output
+
+int main(){
+    std::cout << "Hola Mundo!"; // Imprime string Hola mundo
+
+    return 0; // Retorno codigo de error
+}
