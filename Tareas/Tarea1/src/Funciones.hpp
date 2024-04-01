3bd9f52ff7b2219ac49b19d4e4e9dc40702a1135
Jorge Meneses <menesesjorge2003@gmail.com>
2024-04-01 00:10:58 -0600

    Arreglo de documentacion en Funciones.hpp

--- Tareas/Tarea1/src/Funciones.hpp
+++ Tareas/Tarea1/src/Funciones.hpp
@@ -13,6 +13,7 @@
 
 /**
  * @brief Configura dificultad del juego segun seleccion del ususario
+ * 
  * Esta funcion despliega un menu de dificultad y accede, por medio de punteros, a la variable dif 
  * para configurar la dificultad del juego segun lo que elija el usuario
  * @param dif Dificultad del juego
@@ -21,6 +22,7 @@ void SetDifficulty(int* dif);
 
 /**
  * @brief Inicializa el juego
+ * 
  * Esta funcion configura obtiene una palabra aleatoria del diccionario del juego por medio de punteros, 
  * inicializa el estado de la palabra con puros guines bajos y asigna valor de intentos maximos e intentos 
  * restantes segun la dificultad escogida.
@@ -33,10 +35,11 @@ void Initialize(int currWords, int* dif, string dict[], Ahorcado* ahorcado);
 
 /**
  * @brief Usuario ingresa una letra y se actualiza el estado de la palabra
- * Este tiene un bucle que esta activo si el return de Verification es true. Dentro de este bucle se solicita al 
+ * 
+ * Este tiene un bucle que esta activo si el return de Verification() es true. Dentro de este bucle se solicita al 
  * una letra e itera sobre la palabra a adivinar para ver si la letra ingresada esta. Si la letra ingresada esta, 
  * se actualiza el estado de la palabra cambiando los guines bajos por la letra acertada. Tambien, se lleva conteo 
- * en tiempo real de los intentos restantes. AL momento que el return de Verification sea false, el bucle se 
+ * en tiempo real de los intentos restantes. AL momento que el return de Verification() sea false, el bucle se 
  * detiene totalmente.
  * @param ahorcado Instanciacion de la struct del juego
  * @see Verification
@@ -45,6 +48,7 @@ void Guessing(Ahorcado* ahorcado);
 
 /**
  * @brief Se encarga de verificar el estado
+ * 
  * Compara constantemente el estado de la palabra con la palabra a adivinar para saber cuando llegan a ser iguales;
  * o bien, cuando se agoten los intentos. Funciona como el argumento del ciclo while de Guessing(), pues indica si
  * el juego ya esta en condiciones de terminar o no. Tambien, controla la ultima oportunidad del usuario cuando se 
@@ -57,6 +61,7 @@ bool Verification(Ahorcado* ahorcado);
 
 /**
  * @brief Agrega palabras al diccionario
+ * 
  * Solicita al usuario una palabra para agregarla al diccionario del juego por medio de punteros. Esta palabra
  * es agregada despues de las palabras que ya estaban.
  * @param currWords Cantidad actual de palabras en el diccionario
