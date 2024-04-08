3a683794f1d851e5753263ee72c77fcb2ed320e8
Jorge Meneses <menesesjorge2003@gmail.com>
2024-04-08 09:45:50 -0600

    Linked List comentarios

--- Previos/Previo5/LinkedList.cpp
+++ Previos/Previo5/LinkedList.cpp
@@ -2,33 +2,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*Linked List: Lista de nodos
+
+Cada nodo tiene la informacion y la posicion del siguiente nodo
+*/
+
 class Node{
     public:
-        int value;
-        Node* next;
+        int value; // Data
+        Node* next; // Nodo puntero, direecion del siguiente nodo
 };
 
 int main() {
+    // Instanciaciones nodos
     Node* head;
     Node* one = NULL;
     Node* two = NULL;
     Node* three = NULL; 
 
+    // Posiciones en el heap
     one = new Node();
     two = new Node();
     three = new Node();
 
+    // Asignar valores a values
     one->value = 1;
     two->value = 2;
     three->value = 3;
 
+    // Asignar proximo nodo
     one->next = two;
     two->next = three;
     three->next = NULL;
 
+    // DEfinir cabeza, punto de inicio
     head = one;
+
+    // Flujo de desarrollo, pasa por todos los nodos
     while(head != NULL){
         cout << head->value;
         head = head->next;
     }
+
+    return 0;
 }
