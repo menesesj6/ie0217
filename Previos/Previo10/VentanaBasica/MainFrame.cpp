3c2240b93e6b1955ee8833a1dd86573a450e7681
Jorge Meneses <menesesjorge2003@gmail.com>
2024-05-14 18:54:31 -0600

    Ejemplo ventana basica

---
+++ Previos/Previo10/VentanaBasica/MainFrame.cpp
@@ -0,0 +1,5 @@
+#include "MainFrame.hpp"
+#include <wx/wx.h>
+
+// EStablecer el main frame con su titulo respectivo
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {}
---
+++ Previos/Previo10/VentanaBasica/MainFrame.hpp
@@ -0,0 +1,7 @@
+#include <wx/wx.h>
+
+class MainFrame : public wxFrame{
+    public:
+        // Constructor del main frame
+        MainFrame(const wxString& title);
+};
